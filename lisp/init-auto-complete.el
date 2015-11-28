acd31381fd8aa663db02c1c7b757590d828f639a
Yuanqi Cao <Cao@Yuanqis-MacBook-Pro.local>
2015-11-29 01:03:26 +1100

    Modify the html mode auto complete mode

--- lisp/init-auto-complete.el
+++ lisp/init-auto-complete.el
@@ -61,4 +61,26 @@
 (setq dabbrev-friend-buffer-function 'sanityinc/dabbrev-friend-buffer)
 
 
+;;; Added the auto complete for html
+
+(require-package 'ac-html)
+(defun setup-ac-for-haml ()
+  ;; Require ac-haml since we are setup haml auto completion
+  (require 'ac-haml)
+  ;; Require default data provider if you want to use
+  (require 'ac-html-default-data-provider)
+  ;; Enable data providers,
+  ;; currently only default data provider available
+  (ac-html-enable-data-provider 'ac-html-default-data-provider)
+  ;; Let ac-haml do some setup
+  (ac-haml-setup)
+  ;; Set your ac-source
+  (setq ac-sources '(ac-source-haml-tag
+                     ac-source-haml-attr
+                     ac-source-haml-attrv))
+  ;; Enable auto complete mode
+  (auto-complete-mode))
+
+(add-hook 'haml-mode-hook 'setup-ac-for-haml)
+
 (provide 'init-auto-complete)
