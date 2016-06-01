6bfa525715230602494eab6d597a743b056ee8be
Yuanqi Cao <Cao@Yuanqis-MacBook-Pro.local>
2016-06-01 14:08:12 +1000

    add java snnipts for yasnnips mode

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,4 +1,7 @@
 (require-package 'yasnippet)
 (require 'yasnippet)
 (yas-global-mode 1)
+;;; able yasnippet for java
+(require-package 'java-snippets)
+(require 'java-snippets)
 (provide 'init-yasnippet)
