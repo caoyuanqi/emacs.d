9e75781f2475d0f3f6ce936ce49fabc499f5b4fa
Yuanqi Cao <Cao@Yuanqis-MacBook-Pro.local>
2016-07-14 22:56:10 +1000

    user terminal notificatio for osx notify

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -402,4 +402,32 @@ typical word processor."
     )
   )
 
+;;; use terminal-notifier to notify osx how to have a rest
+(defun notify-osx (title message)
+  (call-process "terminal-notifier"
+                nil 0 nil
+                "-group" "Emacs"
+                "-title" title
+                "-sender" "org.gnu.Emacs"
+                "-message" message
+                "-activate" "oeg.gnu.Emacs"))
+
+(when *is-a-mac*
+  (progn
+    (add-hook 'org-pomodoro-finished-hook
+              (lambda ()
+                (notify-osx "Pomodoro completed!" "Time for a break.")))
+    (add-hook 'org-pomodoro-break-finished-hook
+              (lambda ()
+                (notify-osx "Pomodoro Short Break Finished" "Ready for Another?")))
+    (add-hook 'org-pomodoro-long-break-finished-hook
+              (lambda ()
+                (notify-osx "Pomodoro Long Break Finished" "Ready for Another?")))
+    (add-hook 'org-pomodoro-killed-hook
+              (lambda ()
+                (notify-osx "Pomodoro Killed" "One does not simply kill a pomodoro!")))
+    ))
+
+
+
 (provide 'init-org)
